27d09fb6bb02cee87da9d327b73861341eb5996b
Manuel Dominguez <manuel.dominguez1113@gmail.com>
2022-04-06 15:33:28 -0500

    sessions-and-cookies-exercise

---
+++ src/main/java/codeRunner.java
@@ -0,0 +1,39 @@
+import java.sql.*;
+
+import com.mysql.cj.jdbc.Driver;
+
+
+public class codeRunner {
+    public static void main(String[] args) throws SQLException {
+
+        DriverManager.registerDriver(new Driver());
+        Connection connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/codeup_test_db?allowPublicKeyRetrieval=true&useSSL=false",
+                "root",
+                "codeup"
+        );
+       Statement stmt = connection.createStatement();
+        String selectQuery = "SELECT * FROM albums";
+
+        ResultSet rs = stmt.executeQuery(selectQuery);
+
+        while (rs.next()) {
+            System.out.println("Here's an album:");
+            System.out.println("  id: " + rs.getLong("id"));
+            System.out.println("  artist: " + rs.getString("artist"));
+            System.out.println("  name: " + rs.getString("name"));
+            System.out.println("  date: " + rs.getDate("release_date"));
+        }
+
+        String query = "INSERT INTO albums (artist, name, release_date, genre, sales) VALUES('Nelly Furtado', 'Loose', DATE('2006-05-20'), 'Pop, Urban, R&B', 12.5)";
+        //Statement stmt = connection.createStatement();
+        stmt.executeUpdate(query, Statement.RETURN_GENERATED_KEYS);
+       // ResultSet rs = stmt.getGeneratedKeys();
+        if (rs.next()) {
+            System.out.println("Inserted a new record! New id: " + rs.getLong(1));
+        }
+
+    }
+
+
+}
--- src/main/webapp/WEB-INF/profile.jsp
+++ src/main/webapp/WEB-INF/profile.jsp
@@ -10,6 +10,9 @@
 
     <div class="container">
         <h1>Viewing your profile.</h1>
+        <br>
+        <h2>its empty now..</h2><br>
+        <h3> but one day  this will have your personalized data. isn't that neat?</h3>
     </div>
 
 </body>
