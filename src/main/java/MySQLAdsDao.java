33f990a8f9245a5d9a20c2a34a4d8ac57b544b5c
Manuel Dominguez <manuel.dominguez1113@gmail.com>
2022-04-07 02:52:40 -0500

    completed setting up my data base

--- src/main/java/MySQLAdsDao.java
+++ src/main/java/MySQLAdsDao.java
@@ -1,23 +1,27 @@
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import com.mysql.cj.jdbc.Driver;
+import java.sql.DriverManager;
 
 public class MySQLAdsDao implements Ads{
 private Connection connection;
-private config config;
 
-    public MySQLAdsDao() throws SQLException {
-        config config = new config();
-connection = DriverManager.getConnection(
-        config.getUrl(),
-        config.getUser(),
-        config.getPassword()
-        );
-}
 
     @Override
     public List<Ad> all() throws SQLException {
         List<Ad> ads = new ArrayList<>();
+
+        config config = new config();
+        DriverManager.registerDriver(new Driver());
+        connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/adlister_db?allowPublicKeyRetrieval=true&useSSL=false",
+                "adlister",
+                "codeup"
+                //config.getUrl(),
+                //config.getUser(),
+                //config.getPassword()
+        );
 
         String query = "SELECT * FROM ads";
         Statement stmt = connection.createStatement();
@@ -40,9 +44,18 @@ connection = DriverManager.getConnection(
 
     @Override
     public Long insert(Ad ad) throws SQLException {
-        String query = "INSERT INTO ads ( ?, ?, ?)";
+
+        config config = new config();
+        DriverManager.registerDriver(new Driver());
+        connection = DriverManager.getConnection(
+            "jdbc:mysql://localhost:3306/adlister_db?allowPublicKeyRetrieval=true&useSSL=false",
+            "adlister",
+            "codeup"
+        );
+
+        String query = "INSERT INTO ads (user_id, title, description) VALUES (?, ?, ?);";
         PreparedStatement statement = connection.prepareStatement(query);
-        statement.setLong(1,ad.getUserId());
+        statement.setLong(1,1/*ad.getUserId()*/);
         statement.setString(2,ad.getTitle());
         statement.setString(3,ad.getDescription());
         statement.execute();
